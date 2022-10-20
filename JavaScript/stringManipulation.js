d12cc9c75b7a99ba11f4e6f469796c10695074e3
Vith-MCB <vitorriblacerda@gmail.com>
2022-10-20 17:21:10 -0300

    String manipulation

---
+++ JavaScript/stringManipulation.js
@@ -0,0 +1,6 @@
+var name = "Vitor Ribeiro";
+
+// You can change characters captalizing with functions
+// name.toUpperCase(); /* This function changes all characters to Upper Case*/
+
+name.toLowerCase(); /* This function changes all characters to Lower Case*/

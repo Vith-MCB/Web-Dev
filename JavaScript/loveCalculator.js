1b57d35c29fb0eecee3cf629d0227986fe676fd1
Vith-MCB <vitorriblacerda@gmail.com>
2022-10-25 15:21:49 -0300

    Love calculator

--- JavaScript/loveCalculator.js
+++ JavaScript/loveCalculator.js
@@ -6,7 +6,12 @@ function calculateLove (){
 var love = calculateLove();
 
 if(love > 70){
-    alert("You love each other like Kanye loves Kanye");
-} else{
+    alert("You love each other like Kanye loves Kanye, Your love score is " + love + "%");
+
+}
+else if(love<70 && love > 30){
     alert("Your love score is " + love + "%");
+} 
+else{
+    alert("Your love score is " + love + "%" + ", You go tohether like oil and water");
 }

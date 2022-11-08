66be5023d609c792a14ecffd1de55c953c156ca4
Vith-MCB <vitorriblacerda@gmail.com>
2022-11-08 15:03:27 -0300

    Create bottlesOfBeer.js

---
+++ JavaScript/bottlesOfBeer.js
@@ -0,0 +1,6 @@
+var count = 99;
+while(count > 0){
+    console.log(count + " bottles of beer on the wall, "+count + " bottles of beer.");
+    count--;
+    console.log("Take one down and pass it around, "+ count + " bottles of beer on the wall.");
+}

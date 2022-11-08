764b45da5f090b036d748d4561491ef0ce6dd73f
Vith-MCB <vitorriblacerda@gmail.com>
2022-11-08 15:10:51 -0300

    Update bottlesOfBeer.js

--- JavaScript/bottlesOfBeer.js
+++ JavaScript/bottlesOfBeer.js
@@ -1,6 +1,16 @@
 var count = 99;
 while(count > 0){
-    console.log(count + " bottles of beer on the wall, "+count + " bottles of beer.");
+    if(count != 0){
+        console.log(count + " bottles of beer on the wall, "+count + " bottles of beer.");
+        if(count === 1){
+            console.log("Take one down and pass it around, no more bottles of beer on the wall.");
+        } else {
+            console.log("Take one down and pass it around, "+ count + " bottles of beer on the wall.");
+        }
+    }
     count--;
-    console.log("Take one down and pass it around, "+ count + " bottles of beer on the wall.");
+    if(count === 0){
+        console.log("No more bottles of beer on the wall, no more bottles of beer.");
+        console.log("Go to the store and buy some more, 99 bottles on the wall.")
+    }
 }

4c661b3ac717f25db14761dc2a1aafb6bf7041aa
Vith-MCB <vitorriblacerda@gmail.com>
2022-11-08 15:51:24 -0300

    99 bottles of beer challange

--- JavaScript/bottlesOfBeer.js
+++ JavaScript/bottlesOfBeer.js
@@ -1,16 +1,18 @@
-var count = 99;
-while(count > 0){
-    if(count != 0){
-        console.log(count + " bottles of beer on the wall, "+count + " bottles of beer.");
-        if(count === 1){
-            console.log("Take one down and pass it around, no more bottles of beer on the wall.");
-        } else {
-            console.log("Take one down and pass it around, "+ count + " bottles of beer on the wall.");
+function beer(){
+    var count = 99;
+    while(count > 0){
+        if(count != 0){
+            console.log(count + " bottles of beer on the wall, "+count + " bottles of beer.");
+            if(count === 1){
+                console.log("Take one down and pass it around, no more bottles of beer on the wall.");
+            } else {
+                console.log("Take one down and pass it around, "+ count + " bottles of beer on the wall.");
+            }
         }
-    }
-    count--;
-    if(count === 0){
-        console.log("No more bottles of beer on the wall, no more bottles of beer.");
-        console.log("Go to the store and buy some more, 99 bottles on the wall.")
+        count--;
+        if(count === 0){
+            console.log("No more bottles of beer on the wall, no more bottles of beer.");
+            console.log("Go to the store and buy some more, 99 bottles on the wall.")
+        }
     }
 }
---
+++ JavaScript/fibonacciGenerator.js
@@ -0,0 +1,21 @@
+function fibonacciGenerator (n) {
+    var n1 = 0, n2 = 1, nextTerm;
+    var array = [];
+    if(n >= 1){
+        array.push(n1);
+    }
+    if(n >= 2){
+        array.push(n2);
+    }
+    if(n>2){
+        for(var i = 1; i <= n-2; i++){
+            nextTerm = n1 + n2;
+            array.push(nextTerm);
+            n1 = n2;
+            n2 = nextTerm;
+        }
+    }
+    return array;
+}
+
+    
